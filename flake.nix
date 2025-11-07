8926268fb4149012687040627e19d92225132eb7
Julien Moutinho <julm+bonfire@sourcephile.fr>
2025-11-07 01:58:20 +0100

    fix(nix): run nixfmt
    +maint/clarity
    +maint/auto

--- flake.nix
+++ flake.nix
@@ -2,20 +2,31 @@
   description = "Bonfire self contained build";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     let
       # props to hold settings to apply on this file like name and version
       props = import ./props.nix;
       # set elixir nix version
-      elixir_nix_version = elixir_version:
-        builtins.replaceStrings [ "." ] [ "_" ] "elixir_${elixir_version}";
+      elixir_nix_version =
+        elixir_version: builtins.replaceStrings [ "." ] [ "_" ] "elixir_${elixir_version}";
       erlang_nix_version = erlang_version: "erlangR${erlang_version}";
     in
-    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system:
+    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (
+      system:
       let
         inherit (nixpkgs.lib) optional;
         pkgs = import nixpkgs { inherit system; };
@@ -26,14 +37,16 @@
         version = props.app_version;
 
         # use ~r/erlangR[1-9]+/ for specific erlang release version
-        beamPackages = pkgs.beam.packagesWith
-          pkgs.beam.interpreters.${erlang_nix_version props.erlang_release};
+        beamPackages =
+          pkgs.beam.packagesWith
+            pkgs.beam.interpreters.${erlang_nix_version props.erlang_release};
         # all elixir and erlange packages
         erlang = beamPackages.erlang;
         # use ~r/elixir_1_[1-9]+/ major elixir version
         elixir = beamPackages.${elixir_nix_version props.elixir_release};
-        elixir-ls = beamPackages.elixir_ls.overrideAttrs
-          (oldAttrs: rec { elixir = elixir; });
+        elixir-ls = beamPackages.elixir_ls.overrideAttrs (oldAttrs: rec {
+          elixir = elixir;
+        });
         hex = beamPackages.hex;
 
         # use rebar from nix instead of fetch externally
@@ -46,14 +59,16 @@
         lib = pkgs.lib;
         mix2nix = pkgs.mix2nix;
 
-        installHook = { release }: ''
-          export APP_VERSION="${version}"
-          export APP_NAME="${pname}"
-          export ELIXIR_RELEASE="${props.elixir_release}"
-          runHook preInstall
-          mix release --no-deps-check --path "$out" ${release}
-          runHook postInstall
-        '';
+        installHook =
+          { release }:
+          ''
+            export APP_VERSION="${version}"
+            export APP_NAME="${pname}"
+            export ELIXIR_RELEASE="${props.elixir_release}"
+            runHook preInstall
+            mix release --no-deps-check --path "$out" ${release}
+            runHook postInstall
+          '';
 
         # src of the project
         src = ./.;
@@ -62,14 +77,26 @@
 
         # mix release definition
         release-prod = beamPackages.mixRelease {
-          inherit src pname version mixNixDeps elixir;
+          inherit
+            src
+            pname
+            version
+            mixNixDeps
+            elixir
+            ;
           mixEnv = "prod";
 
           installPhase = installHook { release = "prod"; };
         };
 
         release-dev = beamPackages.mixRelease {
-          inherit src pname version mixNixDeps elixir;
+          inherit
+            src
+            pname
+            version
+            mixNixDeps
+            elixir
+            ;
           mixEnv = "dev";
           enableDebugInfo = true;
           installPhase = installHook { release = "dev"; };
@@ -84,8 +111,12 @@
             name = pname;
             tag = packages.prod.version;
             # required extra packages to make release work
-            contents =
-              [ packages.prod pkgs.coreutils pkgs.gnused pkgs.gnugrep ];
+            contents = [
+              packages.prod
+              pkgs.coreutils
+              pkgs.gnused
+              pkgs.gnugrep
+            ];
             created = "now";
             config.Entrypoint = [ "${packages.prod}/bin/prod" ];
             config.Cmd = [ "version" ];
@@ -155,12 +186,11 @@
             pkgs.cargo
             pkgs.rustc
             (pkgs.postgresql_12.withPackages (p: [ p.postgis ]))
-          ] ++ optional pkgs.stdenv.isLinux
-            pkgs.libnotify # For ExUnit Notifier on Linux.
-          ++ optional pkgs.stdenv.isLinux
-            pkgs.meilisearch # For meilisearch when running linux only
-          ++ optional pkgs.stdenv.isLinux
-            pkgs.inotify-tools; # For file_system on Linux.
+          ]
+          ++ optional pkgs.stdenv.isLinux pkgs.libnotify # For ExUnit Notifier on Linux.
+          ++ optional pkgs.stdenv.isLinux pkgs.meilisearch # For meilisearch when running linux only
+          ++ optional pkgs.stdenv.isLinux pkgs.inotify-tools; # For file_system on Linux.
         };
-      });
+      }
+    );
 }
